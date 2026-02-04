84a3614bedbb22759bc5b5892acf7810cfe91bcb
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2026-02-04 10:45:19 +0800

    FIX: regex for wrapFirstLetter and bodyClass

--- javascripts/discourse/api-initializers/blog-post-styling.js
+++ javascripts/discourse/api-initializers/blog-post-styling.js
@@ -1,18 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 import { SUMMARY_REGEX } from "../components/blog-summary";
 import isBlogTopic from "../lib/is-blog-topic";
-import isMobileDisabled from "../lib/is-mobile-disabled";
-
-const SIZE_CLASSES = {
-  imageFull: "--blog-image-full-width",
-  imageCentered: "--blog-image-centered",
-  noImage: "--blog-no-images",
-};
-
-const POSITION_CLASSES = {
-  aboveTitle: "--blog-image-above-title",
-  belowTitle: "--blog-image-below-title",
-};
 
 function removeSummaryTags(firstPost) {
   if (!firstPost) {
@@ -21,35 +9,13 @@ function removeSummaryTags(firstPost) {
   firstPost.innerHTML = firstPost.innerHTML.replace(SUMMARY_REGEX, "");
 }
 
-function getSizeClass() {
-  switch (settings.image_size) {
-    case "full width":
-      return SIZE_CLASSES.imageFull;
-    case "centered":
-      return SIZE_CLASSES.imageCentered;
-    case "no image":
-      return SIZE_CLASSES.noImage;
-    default:
-      return null;
+function wrapFirstLetter(firstPost) {
+  if (!firstPost || firstPost.querySelector(".blog-post__drop-cap")) {
+    return;
   }
-}
-
-function getPositionClass() {
-  switch (settings.image_position) {
-    case "above title":
-      return POSITION_CLASSES.aboveTitle;
-    case "below title":
-      return POSITION_CLASSES.belowTitle;
-    default:
-      return null;
-  }
-}
-
-function removeStyleClasses() {
-  document.body.classList.remove(
-    ...Object.values(SIZE_CLASSES),
-    ...Object.values(POSITION_CLASSES),
-    "blog-post__drop-cap"
+  firstPost.innerHTML = firstPost.innerHTML.replace(
+    /<p([^>]*)>((?:<(?!\/)[^>]+>)*)([\p{L}\p{N}])/iu,
+    "<p$1>$2<span class='blog-post__drop-cap'>$3</span>"
   );
 }
 
@@ -72,16 +38,6 @@ export default apiInitializer((api) => {
     }
   });
 
-  api.onPageChange(() => {
-    const controller = api.container.lookup("controller:topic");
-    const topic = controller?.model;
-
-    if (!isBlogTopic(topic, settings)) {
-      document.body.classList.remove("blog-post");
-      removeStyleClasses();
-    }
-  });
-
   api.decorateCookedElement(
     (elem, helper) => {
       const post = helper.model;
@@ -96,25 +52,8 @@ export default apiInitializer((api) => {
 
       removeSummaryTags(elem);
 
-      const capabilities = api.container.lookup("service:capabilities");
-      if (isMobileDisabled(capabilities, settings)) {
-        document.body.classList.remove("blog-post");
-        return;
-      }
-
-      document.body.classList.add("blog-post");
-
-      const sizeClass = getSizeClass();
-      const positionClass = getPositionClass();
-      if (sizeClass) {
-        document.body.classList.add(sizeClass);
-      }
-      if (positionClass) {
-        document.body.classList.add(positionClass);
-      }
-
       if (settings.dropcap_enabled) {
-        document.body.classList.add("blog-post__drop-cap");
+        wrapFirstLetter(elem);
       }
     },
     { onlyStream: true }
